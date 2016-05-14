fe6d71dd53b9060af9605a1adb4b6d9fe1a42d3c
Maiko Morales <maiko.moralesr@gmail.com>
2016-05-14 10:20:19 -0400

    added cross origin filter

--- src/main/java/org/gerald/suarez/web/rest/UsuarioResource.java
+++ src/main/java/org/gerald/suarez/web/rest/UsuarioResource.java
@@ -21,6 +21,7 @@ import java.util.Optional;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UsuarioResource {
 
     private final Logger log = LoggerFactory.getLogger(UsuarioResource.class);
