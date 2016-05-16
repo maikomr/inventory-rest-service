ba3927bf245fa08626d95aab82314e40f5414f41
Maiko Morales <maiko.moralesr@gmail.com>
2016-05-16 17:57:09 -0400

    fixed == error comparing two strings

--- src/main/java/org/gerald/suarez/web/rest/AuthResource.java
+++ src/main/java/org/gerald/suarez/web/rest/AuthResource.java
@@ -5,13 +5,12 @@ import org.gerald.suarez.repository.UsuarioRepository;
 import org.gerald.suarez.web.rest.dto.CredentialsDTO;
 import org.gerald.suarez.web.rest.dto.UsuarioDTO;
 import org.gerald.suarez.web.rest.util.HeaderUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.net.URISyntaxException;
@@ -22,7 +21,10 @@ import java.util.Optional;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class AuthResource {
+
+    private final Logger log = LoggerFactory.getLogger(AuthResource.class);
 
     @Inject
     private UsuarioRepository usuarioRepository;
@@ -33,8 +35,10 @@ public class AuthResource {
     public ResponseEntity<Usuario> login(@RequestBody CredentialsDTO credentials) throws URISyntaxException {
         Optional<Usuario> existingUsuario = usuarioRepository.findOneByLogin(credentials.getLogin());
         if (existingUsuario.isPresent()) {
-            if (existingUsuario.get().getPassword() == credentials.getPassword()) {
-
+            if (existingUsuario.get().getPassword().equals(credentials.getPassword())) {
+                return new ResponseEntity<>(existingUsuario.get(),
+                        HeaderUtil.createAlert("Authentication success", "authsucceed"),
+                        HttpStatus.OK);
             } else {
                 return ResponseEntity.badRequest()
                         .headers(HeaderUtil.createFailureAlert("AuthManagement", "invalidpassword", "Invalid Password"))
@@ -45,8 +49,5 @@ public class AuthResource {
                     .headers(HeaderUtil.createFailureAlert("AuthManagement", "invalidlogin", "Invalid Login"))
                     .body(null);
         }
-        return new ResponseEntity<>(existingUsuario.get(),
-                HeaderUtil.createAlert("Authentication success", "authsucceed"),
-                HttpStatus.OK);
     }
 }
